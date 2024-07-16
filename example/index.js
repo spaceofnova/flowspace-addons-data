f311aad49877cebf28a183564cdc3280cafe7fde
Nova Space <mcrealms23@gmail.com>
2024-07-15 23:03:56 -0500

    Update index.js

--- example/index.js
+++ example/index.js
@@ -3,7 +3,19 @@
   // Toast example: 
 
   // Create the event
-  const event = new CustomEvent("toast", { detail: { message: "Example Add-on Loaded!", options: { duration: 1500 } } });
+ const event = new CustomEvent("toast", {
+  detail: {
+    message: "Example Add-on Loaded!",
+    options: {
+      duration: 1500,
+      description: "Example desc",
+      action: {
+        label: "OK",
+        onClick: () => {},
+      },
+    },
+  },
+});
 
   // Send the event
   window.dispatchEvent(event);
