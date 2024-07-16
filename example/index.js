94000d3662210709d3d369323adc27372698ff38
Nova Space <mcrealms23@gmail.com>
2024-07-15 23:02:06 -0500

    Update index.js

--- example/index.js
+++ example/index.js
@@ -1,3 +1,10 @@
 (function() {
   console.log("example addon loaded!");
+  // Toast example: 
+
+  // Create the event
+  const event = new CustomEvent("toast", { detail: { message: "Example Add-on Loaded!", options: { duration: 1500 } } });
+
+  // Send the event
+  window.dispatchEvent(event);
 })();
